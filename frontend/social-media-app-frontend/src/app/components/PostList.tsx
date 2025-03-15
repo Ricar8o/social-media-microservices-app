11feb63509e2947799d1a6944abd573d11b91d7f
Ricar8o <45279329+Ricar8o@users.noreply.github.com>
2025-03-14 22:31:37 -0500

    feat: update layout title and implement PostList component for displaying posts

---
+++ frontend/social-media-app-frontend/src/app/components/PostList.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import Link from "next/link"
+
+export default function PostList() {
+  const [posts, setPosts] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        // Fetch posts
+        const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+        const postsData = await postsResponse.json()
+
+        setPosts(postsData)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchPosts()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="loading-container">
+        <div className="loading-card"></div>
+        <div className="loading-card"></div>
+        <div className="loading-card"></div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="posts-container">
+    </div>
+  )
+}
+
