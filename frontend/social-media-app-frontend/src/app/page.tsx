9f66d8f0aa0d3c4b578a1edcc4f7e5ecbe2cf41d
Ricar8o <45279329+Ricar8o@users.noreply.github.com>
2025-03-15 00:43:57 -0500

    feat: implement user authentication with login/logout functionality

--- frontend/social-media-app-frontend/src/app/page.tsx
+++ frontend/social-media-app-frontend/src/app/page.tsx
@@ -1,16 +1,42 @@
+"use client"
+
 import Link from "next/link"
 import PostList from "./components/PostList"
-
+import { useEffect, useState } from "react"
 
 export default function Home() {
+
+  const [credentials, setCredentials] = useState<any>(null)
+
+  useEffect(() => {
+    const credentials = localStorage.getItem("credentials")
+    if (!credentials) {
+      return
+    }
+    const parsedCredentials = JSON.parse(credentials)
+    setCredentials(parsedCredentials)
+  }, [])
+
+  const handleLogout = () => {
+    localStorage.clear()
+    window.location.reload()
+    console.log("Logged out")
+  }
+
   return (
     <main className="container">
       <div className="header">
         <h1>Social Feed</h1>
         <div className="nav-buttons">
-          <Link href="/login" className="button outline">
+          {!credentials ? (
+            <Link href="/login" className="button outline">
             Login
           </Link>
+          ) : (
+            <button type="button" className="button outline" onClick={handleLogout}>
+              Logout
+            </button>
+          )}
           <Link href="/profile" className="button">
             Profile
           </Link>
