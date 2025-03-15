4c70388ff68bbf04c8a345ccdd7e0f63956cd1c0
Ricar8o <45279329+Ricar8o@users.noreply.github.com>
2025-03-14 22:54:58 -0500

    feat: add CORS support to posts and users microservices

--- backend/microservices/posts-mc/src/index.ts
+++ backend/microservices/posts-mc/src/index.ts
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 import postsRoutes from "./routes/posts.routes";
 import feedRoutes from "./routes/feed.routes";
 import { authorizer } from "./middlewares/authorizer.middleware";
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/posts', authorizer, postsRoutes);
 app.use('/feed', authorizer, feedRoutes);
--- backend/microservices/users-mc/src/index.ts
+++ backend/microservices/users-mc/src/index.ts
@@ -4,11 +4,13 @@ import authRoutes from "./routes/auth.routes";
 import usersRoutes from "./routes/users.routes";
 import userRoutes from "./routes/user.routes";
 import { authorizer } from "./middlewares/authorizer.middleware";
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/users', authorizer, usersRoutes);
